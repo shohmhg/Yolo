f23eae568bbc53744740c05d614f960e0c5427f9
shohmhg <53076098+shohmhg@users.noreply.github.com>
2019-07-24 23:29:54 +0900

    go

--- location.js
+++ location.js
@@ -1,6 +1,8 @@
 const API_KEY = "ec091fd0e142b0fba2c3ca211c831486";
 const LOCATION = "location";
 
+const weather = document.querySelector('.wl');
+
 function suc(position){
     const loco = {
         latitude : position.coords.latitude,
@@ -14,18 +16,32 @@ function err(){
     console.log("errorcallloaction");
 }
 
+function getWeather(lat, lon){
+    fetch(
+         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+         ).then(function(response){
+             return response.json();
+         }).then(function(json){
+             const temperature = json.main.temperature;
+             const name = json.main.name;
+
+             weather.innerText = `${temperature} @ ${name}`;
+         });
+}
+
 function saveLocation(){ 
     navigator.geolocation.getCurrentPosition(suc, err);
     //localStorage.setItem(LOCATION, ));
-
-    //latitude
-    //longitude
 
     console.log("saveLocation");
 }
 
 function loadLocation(){
+    const currentlocation = localStorage.getItem(LOCATION);
+    const cl = JSON.parse(currentlocation);
+    getWeather(cl.latitude, cl.longitude);
 
+    console.log("loadLocation", currentlocation, JSON.parse(currentlocation));
 }
 
 function init(){
